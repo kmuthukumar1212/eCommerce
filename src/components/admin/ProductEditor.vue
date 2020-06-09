8a22f31290e79c7922035e6b790d109b04277831
voja <voja@gmail.com>
2020-06-09 08:57:08 +0200

    29

---
+++ src/components/admin/ProductEditor.vue
@@ -0,0 +1,90 @@
+<template>
+    <div>
+        <h2 class="text-center p-2">
+            {{ editMode ? "Edit Product" : "Add Product" }}
+        </h2>
+
+        <div class="form-group">
+            <label for="">Name</label>
+            <input type="text" class="form-control" v-model="product.name" />
+        </div>
+
+        <div class="form-group">
+            <label for="">Description</label>
+            <textarea
+                class="form-control"
+                v-model="product.description"
+            ></textarea>
+        </div>
+
+        <div class="form-group">
+            <label for="">Price</label>
+            <input type="text" class="form-control" v-model="product.price" />
+        </div>
+
+        <div class="form-group">
+            <label for="">Image</label>
+            <input type="file" class="form-control" @change="onFileSelected" />
+        </div>
+
+        <div class="form-group">
+            <label for="">Category</label>
+            <select class="form-control" @change="onChange">
+                <option :value="null">Choose a category</option>
+                <option
+                    v-for="(c, i) in categories"
+                    :key="i"
+                    :value="c.id"
+                    :selected="c.id == product.category.id"
+                >
+                    {{ c.name }}
+                </option>
+            </select>
+        </div>
+
+        <div class="text-center">
+            <router-link to="/admin/products" class="btn btn-secondary m-1"
+                >Cancel</router-link
+            >
+            <button class="btn btn-primary m-1" @click="handleProduct">
+                {{ editMode ? "Edit" : "Add" }}
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapState, mapActions, mapMutations } from "vuex";
+
+import ProductPagination from "../ProductPagination";
+
+export default {
+    data() {
+        return {
+            product: {
+                name: null,
+                description: null,
+                price: null,
+                image: null,
+                category: null,
+            },
+        };
+    },
+    computed: {
+        ...mapState(["categories"]),
+        editMode() {
+            return this.$route.params["op"] == "edit";
+        }
+    },
+    methods: {
+        onFileSelected(e) {
+            this.product.image = e.target.files[0];
+        },
+        onChange(e) {
+            this.product.category = e.target.value;
+        }
+    },
+    created() {
+    },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import Thanks from "../components/Thanks";
 
 import Admin from "../components/admin/Admin";
 import Products from "../components/admin/Products";
+import ProductEditor from "../components/admin/ProductEditor";
 
 Vue.use(VueRouter);
 
@@ -23,7 +24,14 @@ export default new VueRouter({
         {
             path: "/admin",
             component: Admin,
-            children: [{ path: "products", component: Products }],
+            children: [
+                { path: "products", component: Products },
+                {
+                    path: "products/:op(add|edit)/:id(\\d+)?",
+                    component: ProductEditor,
+                },
+                { path: "*", redirect: "/admin/products" },
+            ],
         },
 
         { path: "/:slug?", component: Pages },
